4cef6a05126c1ab9c1cd9c5cfed59bfa2e64e237
Dhruv Jawalkar <dhruv.jawalkar@gmail.com>
2024-09-06 13:51:44 -0700

    Added basic test cases for storing a string in the redis instance as a key value pair using the Jedis, JedisPool clients.

--- src/test/java/com/redislabs/university/RU102J/examples/HelloTest.java
+++ src/test/java/com/redislabs/university/RU102J/examples/HelloTest.java
@@ -12,6 +12,7 @@ import static org.hamcrest.Matchers.is;
 public class HelloTest {
 
     @Test
+    //basic test case to store and retrieve a string (with key: "hello", value: "world") in redis using the Jedis client
     public void sayHelloBasic() {
         Jedis jedis = new Jedis(HostPort.getRedisHost(), HostPort.getRedisPort());
 
@@ -26,6 +27,8 @@ public class HelloTest {
     }
 
     @Test
+    //basic test case to store and retrieve a string (with key: "hello", value: "world") in redis using the Jedis client
+    //matching assertions on set() operation and get() result
     public void sayHello() {
         Jedis jedis = new Jedis(HostPort.getRedisHost(), HostPort.getRedisPort());
 
@@ -42,6 +45,9 @@ public class HelloTest {
     }
 
     @Test
+    //basic test case to store and retrieve a string (with key: "hello", value: "world") in redis but in a thread safe manner using the JedisPool client (it uses a connection pool)
+    //matching assertions on set() operation and get() result
+    //.close() operation closes the TCP socket connection with redis instance
     public void sayHelloThreadSafe() {
         JedisPool jedisPool;
 
